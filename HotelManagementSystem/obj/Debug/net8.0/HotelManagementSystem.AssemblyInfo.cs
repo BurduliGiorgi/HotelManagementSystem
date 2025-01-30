956ca776ab677a7e051b418f44c16a054e5d4686
myusername <burdulig25@gmail.com>
2025-01-30 19:02:22 +0400

    add css code in hotel details.cshtml

--- HotelManagementSystem/obj/Debug/net8.0/HotelManagementSystem.AssemblyInfo.cs
+++ HotelManagementSystem/obj/Debug/net8.0/HotelManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d67c9c2dcc7bc0892170fb29d0bb505cf3a3415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa40558ac6dd1bc9b71779f270abc43389693b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
