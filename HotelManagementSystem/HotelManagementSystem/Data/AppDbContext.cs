c6912f0f796dd810dbacc9e7b110d20d386a90a5
BurduliGiorgi <burdulig25@gmail.com>
2025-03-28 22:09:08 +0400

    Create Login,Registration,Email Verify and Forgot Password Pages

---
+++ HotelManagementSystem/HotelManagementSystem/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementSystem.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
